4b9b041e0d3adb27d78ecc0fec9ddf9db9688baa
hylim5 <your@email.com>
2022-05-05 09:12:59 +0800

    modified

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col>1</Col>
+        <Col>2</Col>
+      </Row>
+      <Row>
+        <Col>1</Col>
+        <Col>2</Col>
+        <Col>3</Col>
+      </Row>
+      <Row>
+        <Col sm = {8} >1</Col>
+        <Col sm = {4} >2</Col>
+      </Row>
+      <Row>
+        <Col>1</Col>
+        <Col>
+        <Row>
+          <Col>2</Col>
+        </Row>
+        <Row>
+          <Col>3</Col>
+        </Row>
+        </Col>
+      </Row>
+      <br></br>
+      <Row>
+        <Col>1</Col>
+      </Row>
+      <Row>
+        <Col>2</Col>
+        <Col>3</Col>
+      </Row>
+      <Row>
+        <Col>4</Col>
+      </Row>
+    </Container>
   );
 }
 
